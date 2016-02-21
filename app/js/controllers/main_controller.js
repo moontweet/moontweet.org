d1763b2b387b38ab3480d68fac9bc56960e967a2
losalongiles <alongi.andrea@gmail.com>
2016-02-21 17:26:53 +0100

    visual fixing

--- app/js/controllers/main_controller.js
+++ app/js/controllers/main_controller.js
@@ -2,7 +2,7 @@ var app = angular.module("t2m");
 
 app.controller("mainController", ['$scope', '$rootScope', '$state', function($scope, $rootScope, $state) {
     $scope.$state = $state;
-    $scope.tweetText = "Hi Mom! I'm on the moon! \n\nLorem Ipsum Lorem  bla bla bla Ipsum Lorem  bla bla bla scemo chi legge!";
+    $scope.tweetText = "";
 
     $scope.openMobileEditor = function() {
         $rootScope.$broadcast('mobile:editor:open')
