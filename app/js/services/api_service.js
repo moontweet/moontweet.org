b18fa85136e68bb6428ecf53228b189244e95b7b
Marco Andreotti <theclash89@gmail.com>
2017-02-04 22:47:50 +0100

    Changed api hostname

--- app/js/services/api_service.js
+++ app/js/services/api_service.js
@@ -2,7 +2,7 @@ var app = angular.module("t2m");
 
 app.factory("Api", [function() {
     return {
-        baseUrl: "http://149.202.8.167:32773",
+        baseUrl: "http://api.moontweet.org",
         getUrl: function(path) {
             return this.baseUrl + path;
         }
